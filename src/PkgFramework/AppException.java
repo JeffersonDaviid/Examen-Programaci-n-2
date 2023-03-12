541148c1b4ebc4dd551c12a5053fe7dcecb191a7
Jefferson David <jeffersonchile2001@gmail.com>
2023-03-12 14:35:32 -0500

    errores

--- src/PkgFramework/AppException.java
+++ src/PkgFramework/AppException.java
@@ -3,26 +3,20 @@ package PkgFramework;
 public class AppException extends Exception {
 
     public AppException(Throwable cause, Object className, String message) {
-        // setDebuggingLog();
         System.out.println(
-                "\nError causado por:\t" + cause + "\nEn: \t\t\t" + className + "\nMensaje:\t\t" + message);
+                "\nError causado por:\t" + cause +
+                        "\nEn: \t\t\t" + className +
+                        "\nMensaje:\t\t" + message);
     }
 
     public AppException(Throwable cause, Object className) {
-        // setDebuggingLog();
         System.out.println(
-                "\nError causado por:\t" + cause + "\nEn: \t\t\t" + className);
+                "\nError causado por:\t" + cause +
+                        "\nEn: \t\t\t" + className);
     }
 
     public AppException(String message) {
-        System.out.println(
-                "\nMensaje:\t\t" + message);
-        setDebuggingLog();
+        System.out.println("\nMensaje:\t\t" + message);
     }
 
-    void setDebuggingLog() {
-        System.out.println("{AppException}");
-        System.out.println(getMessage());
-        System.out.println(getCause());
-    }
 }
